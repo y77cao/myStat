90db96c08a64ba35945c5b400108a895f31649b9
Alice Cao <acao@acao.local>
2018-01-29 16:19:49 -0500

    Beautify

--- src/browser_action/rendercharts.js
+++ src/browser_action/rendercharts.js
@@ -1,5 +1,6 @@
 function renderBars(data) {
 	var myChart = Highcharts.chart('container', {
+		colors: ['#4ABDAC', '#FC4A1A', '#F7B733', '#DFDCE3'],
         chart: {
             type: 'bar'
         },
@@ -7,12 +8,18 @@ function renderBars(data) {
             text: 'Website Visits'
         },
         xAxis: {
-            type: 'category'
+            type: 'category',
+            labels: {
+            	align: 'left',
+            	x: 2,
+            	y: -10
+            },
         },
         yAxis: {
             title: {
                 text: 'Websites'
-            }
+            },
+            gridLineWidth: 0.5
         },
         plotOptions: {
            series: {
@@ -20,8 +27,17 @@ function renderBars(data) {
                dataLabels: {
                    enabled: true,
                    format: '{point.y:.1f}%'
-               }
-             }
+               },
+               showInLegend: false
+             },
+            bar: {
+               colorByPoint: true,
+               pointwidth: 5,
+               pointPadding: 0.3
+            }
+       },
+       tooltip: {
+       	pointFormat: '<span style="color:{point.color}">\u25CF</span> Count: <b>{point.y}</b><br/>'
        },
         series: [{
             data: data
